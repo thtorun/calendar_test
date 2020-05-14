78ac7a3507398bdf8516d3eef4894093e8bc1c41
thtorun <64398517+thtorun@users.noreply.github.com>
2020-05-14 11:34:20 +0600

    Add files via upload
    1st commit

---
+++ cal.py
@@ -0,0 +1,10 @@
+import calendar
+print(calendar.weekheader(4))
+print()
+
+print(calendar.firstweekday())
+print()
+
+print(calendar.month(2020 , 1 ))
+print(calendar.monthcalendar(2020, 1))
+print(calendar.calendar(2020))
